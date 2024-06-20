dea57cee4641a7c09b41de02797dc08f35a2f6a0
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2024-06-19 20:20:45 -0400

    Improved Mixins, Fixed Upload to Imgur button, Added auto-encoding data if it doesn't fit into the sign

--- src/main/java/com/nettakrim/signed_paintings/gui/UIHelper.java
+++ src/main/java/com/nettakrim/signed_paintings/gui/UIHelper.java
@@ -29,6 +29,7 @@ public class UIHelper {
     
     private static final int SPACING_X = BUTTON_WIDTH - INPUT_SLIDER_TEXT - INPUT_SLIDER_SLIDER - 1;
     private static final int SPACING_Y = 18;
+    private static final int Y_OFF = 55;
     
     private static final int AREA_SIZE = BUTTON_HEIGHT * 2 + 5;
 
@@ -80,22 +81,21 @@ public class UIHelper {
 
         // LEFT
         createCenteringButtons();
-        int yOff = 55;
-        createButton(PADDING, yOff, (BUTTON_WIDTH - SPACING_X) / 2, Text.translatable(SignedPaintingsClient.MODID + ".copy_url"), UIHelper::copyURL);
-        createButton(MathHelper.ceil(PADDING + (BUTTON_WIDTH + SPACING_X) / 2f), yOff, (BUTTON_WIDTH - SPACING_X) / 2, Text.translatable(SignedPaintingsClient.MODID + ".copy_data"), UIHelper::copyData);
+        createButton(PADDING, Y_OFF, (BUTTON_WIDTH - SPACING_X) / 2, Text.translatable(SignedPaintingsClient.MODID + ".copy_url"), UIHelper::copyURL);
+        createButton(MathHelper.ceil(PADDING + (BUTTON_WIDTH + SPACING_X) / 2f), Y_OFF, (BUTTON_WIDTH - SPACING_X) / 2, Text.translatable(SignedPaintingsClient.MODID + ".copy_data"), UIHelper::copyData);
 
-        inputSliders[3] = createInputSlider(PADDING, getYPosition(yOff, 1.25f), SignedPaintingsClient.MODID + ".offset_x", -8f, 8f, 0.25f, -64f, 64f, offsetVec.x);
+        inputSliders[3] = createInputSlider(PADDING, getYPosition(Y_OFF, 1.25f), SignedPaintingsClient.MODID + ".offset_x", -8f, 8f, 0.25f, -64f, 64f, offsetVec.x);
         inputSliders[3].setOnValueChanged(UIHelper::onXOffsetSliderChanged);
-        inputSliders[4] = createInputSlider(PADDING, getYPosition(yOff, 2.25f), SignedPaintingsClient.MODID + ".offset_y", -8f, 8f, 0.25f, -64f, 64f, offsetVec.y);
+        inputSliders[4] = createInputSlider(PADDING, getYPosition(Y_OFF, 2.25f), SignedPaintingsClient.MODID + ".offset_y", -8f, 8f, 0.25f, -64f, 64f, offsetVec.y);
         inputSliders[4].setOnValueChanged(UIHelper::onYOffsetSliderChanged);
-        inputSliders[5] = createInputSlider(PADDING, getYPosition(yOff, 3.25f), SignedPaintingsClient.MODID + ".offset_z", -8f, 8f, 0.25f, -64f, 64f, offsetVec.z);
+        inputSliders[5] = createInputSlider(PADDING, getYPosition(Y_OFF, 3.25f), SignedPaintingsClient.MODID + ".offset_z", -8f, 8f, 0.25f, -64f, 64f, offsetVec.z);
         inputSliders[5].setOnValueChanged(UIHelper::onZOffsetSliderChanged);
 
-        inputSliders[6] = createInputSlider(PADDING, getYPosition(yOff, 4.5f), SignedPaintingsClient.MODID + ".rotation_x", -180f, 180f, 22.5f, -360f, 360f, rotationVec.x);
+        inputSliders[6] = createInputSlider(PADDING, getYPosition(Y_OFF, 4.5f), SignedPaintingsClient.MODID + ".rotation_x", -180f, 180f, 22.5f, -360f, 360f, rotationVec.x);
         inputSliders[6].setOnValueChanged(UIHelper::onXRotationSliderChanged);
-        inputSliders[7] = createInputSlider(PADDING, getYPosition(yOff, 5.5f), SignedPaintingsClient.MODID + ".rotation_y", -180f, 180f, 22.5f, -360f, 360f, rotationVec.y);
+        inputSliders[7] = createInputSlider(PADDING, getYPosition(Y_OFF, 5.5f), SignedPaintingsClient.MODID + ".rotation_y", -180f, 180f, 22.5f, -360f, 360f, rotationVec.y);
         inputSliders[7].setOnValueChanged(UIHelper::onYRotationSliderChanged);
-        inputSliders[8] = createInputSlider(PADDING, getYPosition(yOff, 6.5f), SignedPaintingsClient.MODID + ".rotation_z", -180f, 180f, 22.5f, -360f, 360f, rotationVec.z);
+        inputSliders[8] = createInputSlider(PADDING, getYPosition(Y_OFF, 6.5f), SignedPaintingsClient.MODID + ".rotation_z", -180f, 180f, 22.5f, -360f, 360f, rotationVec.z);
         inputSliders[8].setOnValueChanged(UIHelper::onZRotationSliderChanged);
 
         //RIGHT
@@ -113,7 +113,7 @@ public class UIHelper {
         inputSliders[2].setOnValueChanged(UIHelper::onPixelSliderChanged);
         createButton(-PADDING, getYPosition(0, 5.25f), BUTTON_WIDTH, getBackgroundText(isBackgroundEnabled), UIHelper::cycleBackground);
 
-        createButton(-PADDING, getYPosition(yOff, 6.5f), BUTTON_WIDTH, Text.translatable("gui.done"), (ButtonWidget a) -> screen.close());
+        createButton(-PADDING, getYPosition(Y_OFF, 6.5f), BUTTON_WIDTH, Text.translatable("gui.done"), (ButtonWidget a) -> screen.close());
     }
 
     private static void createCenteringButtons() {
@@ -138,6 +138,12 @@ public class UIHelper {
                 .build();
 
         buttons.add(widget);
+    }
+
+    public static ButtonWidget createUploadButton(ButtonWidget.PressAction pressAction) {
+        return ButtonWidget.builder(Text.translatable(SignedPaintingsClient.MODID + ".upload_prompt"), pressAction)
+                .position(getAlignedOffset(-PADDING, BUTTON_WIDTH), PADDING + getYPosition(Y_OFF, 5.5f))
+                .size(BUTTON_WIDTH, BUTTON_HEIGHT).build();
     }
 
     private static int getCenteringButtonPosition(int size, Centering.Type centering, int buttonSize, int screenSize) {
